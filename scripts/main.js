bf5c28b53857e4cf8ac030b318d51150bcdc116c
sharrie <sharrie.s.chen@gmail.com>
2018-01-06 17:31:21 -0800

    better tabbed nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
-$(document).ready(function(event){
-  $("#ul-menu-list li").click(function () {
-      $('.prog-content').hide().eq($(this).index()).show();
-  });
-  $('#ul-menu-list li:first').trigger('click'); // Default
-});
+$(document).ready(function(){
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
